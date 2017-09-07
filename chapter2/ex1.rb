9d7697411e659be2ff3a9172d7f999d988e4ac54
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-07 18:22:57 +0100

    Chapter 2 commit

---
+++ chapter2/ex1.rb
@@ -0,0 +1,10 @@
+puts 'Hello, world!'
+puts
+puts 'Good-bye.'
+puts 'I like' + 'apple pie'
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+puts
+puts 'blink ' * 4
+puts
+puts 'You\'re swell!'
