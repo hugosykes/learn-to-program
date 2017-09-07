5d69e116903c6eaa0559b4647955efc7c64db33c
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-07 18:56:37 +0100

    Chapter 4 commit

---
+++ chapter4/ex1.rb
@@ -0,0 +1,29 @@
+var1 = 2
+var2 = '5'
+puts 
+puts var1.to_s + var2
+puts var1 + var2.to_i
+puts
+puts "'15'.to_f"
+puts '15'.to_f
+puts
+puts "'99.999'.to_f"
+puts '99.999'.to_f
+puts
+puts "'99.999'.to_i"
+puts '99.999'.to_i
+puts
+puts "'5 is my favourite number!'.to_i"
+puts '5 is my favourite number!'.to_i
+puts
+puts "'Who asked you about 5 or whatever?'.to_i"
+puts 'Who asked you about 5 or whatever?'.to_i
+puts
+puts "'your momma did.'.to_f"
+puts 'your momma did.'.to_f
+puts
+puts "'stringy'.to_s"
+puts 'stringy'.to_s
+puts
+puts "3.0.to_i"
+puts 3.0.to_i
