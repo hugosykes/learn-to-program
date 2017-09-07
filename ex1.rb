0430ef756dad5469e569c6e4683cddeddf371e8a
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-07 18:13:18 +0100

    First commit - chapter 1

---
+++ ex1.rb
@@ -0,0 +1,23 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts "How many hours are in a year?"
+puts 24*365
+puts
+puts "How many minutes are in a decade?"
+puts 60*24*365*10
+puts 
+puts "How many seconds old am I? (Assuming 30 days in a month)"
+puts (4+(23*12))*60*60*24*30
+puts
+puts "How many chocolates do I plan on eating in my life? Not that many"
+puts
+puts "If I'm 1246 million seconds old, how old am I?"
+puts "Well there are 60*60*24*365 seconds in a year so: #{1246000000.0/(60.0*60.0*24.0*365.0)} years"
