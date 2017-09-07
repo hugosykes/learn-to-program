e73a43d0f88bddda786031f59ccd55b7b392d3c7
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-07 18:29:34 +0100

    Chapter 3 commit

---
+++ chapter3/ex1.rb
@@ -0,0 +1,6 @@
+name = "Hugo Sykes"
+puts "Hello "+name+", how are you?"
+puts
+composer1 = "Mozart"
+composer2 = "Beethoven"
+puts "My favourite composer is " + composer1 + " but I also enjoy the works of " + composer2 + " enormously!"
